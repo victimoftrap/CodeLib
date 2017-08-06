af706e0becd2287579f0f6607f09d7de6d1c1c33
TagartOmsk <tagart.ru@mail.ru>
2017-08-06 20:16:46 +0600

    Developed Queue

--- CodeLib/Queue.cpp
+++ CodeLib/Queue.cpp
@@ -6,12 +6,12 @@ Queue::Queue ()
 
 int Queue::pop ()
 {
-    if (_root != nullptr) 
+    if (root != nullptr) 
     {
-        int var = _root->value;
-        Element* _temp = _root;
-        _root = _root->next;
-        delete _temp;
+        int var = root->value;
+        Element* temp = root;
+        root = root->next;
+        delete temp;
         return var;
     }
     return 0;//throw error
@@ -19,18 +19,15 @@ int Queue::pop ()
 
 void Queue::push (int value)
 {
-    if (_root == nullptr) 
+    if (root == nullptr) 
     {
-        _root = new Element(value,nullptr);
+        root = new Element(value,nullptr);
+        current = root;
     }
     else 
     {
-        _current = _root;
-        while (_current->next != nullptr) 
-        {
-            _current = _current->next;
-        }
-
+        current->next = new Element (value, nullptr);
+        current = current->next;
     }
 }
 
@@ -40,3 +37,81 @@ Queue::Element::Element (int value, Element* next)
     this->next = next;
 }
 
+int Queue::get ()
+{
+    return root->value;
+}
+
+bool Queue::isEmpty ()
+{
+    if (root == nullptr)return true;
+    else return false;
+}
+
+std::string Queue::toString ()
+{
+    std::string result="";
+    char a[10];
+    if (isEmpty ())return result;
+    else 
+    {
+        current = root;
+        while (current->next != nullptr) 
+        {
+            _itoa_s (current->value, a, 10);
+            result += a;
+            result += " ";
+            current = current->next;
+        }
+        _itoa_s (current->value, a, 10);
+        result += a;
+        result += " ";
+    }
+    return result;
+}
+
+void Queue::reverse ()
+{
+    Element* temp = nullptr;
+    Element* temp2 = nullptr;
+    bool isLast = false;
+    bool first = true;
+    while (!isLast)
+    {
+        if (root == current)isLast = true;
+        if (first)
+        {
+            temp = root;
+            root = root->next;
+            temp2 = root->next;
+            root->next = temp;
+            temp->next = nullptr;
+            temp = root;
+            first = false;
+        }
+        else
+        {
+            root = temp2;
+            temp2 = temp2->next;
+            if (root == current)isLast = true;
+            root->next = temp;
+            temp = root;
+        }
+    }    
+    current = root;
+    while (current->next != nullptr)current = current->next;
+}
+
+Queue::~Queue ()
+{
+    if (root != nullptr)
+    {
+        while (root->next != nullptr)
+        {
+            Element* temp = root;
+            root = root->next;
+            delete temp;
+        }
+        delete root;
+    }
+}
